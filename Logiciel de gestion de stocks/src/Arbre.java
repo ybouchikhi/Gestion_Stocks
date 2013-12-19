b10d11aa0e5701bca201b498fa2eaa0d70c7bf31
cdoucour <clara.doucoure@eleves.ec-nantes.fr>
2013-12-19 18:55:32 +0100

    Update Arbre.java

--- Logiciel de gestion de stocks/src/Arbre.java
+++ Logiciel de gestion de stocks/src/Arbre.java
@@ -33,7 +33,7 @@ public class Arbre extends JTree{
 	 * Parametre en entree : category
 	 */
 	private DefaultMutableTreeNode buildTree(Category cat){
-		//Création d'une racine initiale
+		//Crï¿½ation d'une racine initiale
 		DefaultMutableTreeNode racine = new DefaultMutableTreeNode(cat);
 		for (Category child : cat.children){
 			racine.add(buildTree(child));
@@ -47,35 +47,6 @@ public class Arbre extends JTree{
 
 
 
-	/*
-	 * Methode permettant de supprimer une categorie de l'arbre
-	 */
-	public void removeCategoryFromTree(Category cat){
-		/*
-		 * A completer
-		 */
-
-	}
-
-
-	/*
-	 * Methode permettant d'ajouter une categorie a l'arbre
-	 * Paramètre d'entree : 
-	 *                         categorie mere categorieMerea laquelle rattacher la categorie a rajouter
-	 *                         categorie a rajouter
-	 */
-	public void addCategoryToTree(Category categorieMere, Category categorie){
-		/*
-		 * A completer
-		 */
-
-	}
-
-
-	/*
-	 * 
-	 * 
-	 */
 	public DefaultMutableTreeNode findNode(Category cat){
 
 		DefaultMutableTreeNode del = new DefaultMutableTreeNode();
